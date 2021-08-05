d114d29b88d24c49982df7c12ca727e64fc828de
Viktoriia <77969607+Viktorika-p@users.noreply.github.com>
2021-08-05 11:40:50 -0700

    Add files via upload

---
+++ Homepage/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header () {
+    return (
+        <header className="App-header">
+            <div>Code Arena</div>
+            <Navigation />
+        </header>
+    )
+}
+
+export default Header;
